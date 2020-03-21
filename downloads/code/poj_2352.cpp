ad6b4227d2614de163f87496cc535e21b6fb09eb
davidsonic <davidsonic@163.com>
2020-03-21 03:57:15 -0400

    Site updated: 2020-03-21 03:57:05

---
+++ downloads/code/poj_2352.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+const int N = 15010;
+
+int n;
+vector<pair<int,int> > coords;
+
+bool cmp(const pair<int,int>&p1, const pair<int,int>&p2){
+    if(p1.second!=p2.second){
+        return p1.second<p2.second;
+    }else return p1.first<p2.first;
+}
+
+const int M = 32001;
+int c[M+2];
+int lowbit(int x){
+    return x & -x;
+}
+
+int ans[N];
+
+void add(int x, int d){
+    for(int i=x;i<=M;i+=lowbit(i)){
+        c[i] += d;
+    }
+}
+
+int query(int x){
+    int ans = 0;
+    for(int i=x;i>0;i-=lowbit(i)){
+        ans += c[i];
+    }
+    return ans;
+}
+
+int main(){
+    scanf("%d", &n);
+    for(int i=1;i<=n;i++){
+        int x, y;
+        scanf("%d%d", &x, &y);
+        coords.push_back(make_pair(x, y));
+    }
+    sort(coords.begin(), coords.end(), cmp);
+
+    for(int i=0;i<coords.size();i++){
+        add(coords[i].first, 1);
+        ans[query(coords[i].first)] ++;
+    }
+
+    for(int i=1;i<=n;i++){
+        printf("%d\n", ans[i]);
+    }
+    return 0;
+}
